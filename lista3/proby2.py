349d912dfbc8640663401c0821449ef08602a4ca
AHerda <92255793+AHerda@users.noreply.github.com>
2023-05-30 03:21:52 +0200

    Update proby2.py

--- lista3/proby2.py
+++ lista3/proby2.py
@@ -27,6 +27,10 @@ def backpropagation(input_data, target, weights_hidden, weights_output, learning
     """Algorytm propagacji wstecznej"""
     X = np.array(input_data, ndmin=2).T
     y = np.array(target, ndmin=2).T
+
+    print("+" + "=" * 10 + "+" + "=" * 10 + "+")
+    print(f"|{'Epoch':^10}|{'Accuracy':^10}|")
+    print("+" + "=" * 10 + "+" + "=" * 10 + "+")
     
     for epoch in range(epochs):
         # Przekształć dane przez sieć neuronową
@@ -48,16 +52,28 @@ def backpropagation(input_data, target, weights_hidden, weights_output, learning
         
         # Zaktualizuj wagi dla warstwy ukrytej
         weights_hidden += learning_rate * np.dot(hidden_delta, X.T)
+
+        accuracy = 0
+        for i in range(len(target)):
+            if target[i] == int(output.flatten()[i]):
+                accuracy += 1
+        accuracy /= 1000
+
+        print(f"|{epoch + 1:^10}|{accuracy:^10.2f}|")
+        print("+" + "-" * 10 + "+" + "-" * 10 + "+")
     
     return weights_hidden, weights_output
 
 # Dane wejściowe
 input_data = (2 * np.random.random_sample((1000, 2)) - 1)
+input_data[input_data == 0] = 1
+input_data_l1 = input_data / np.linalg.norm(input_data, ord=1)
+input_data_l2 = input_data / np.linalg.norm(input_data, ord=2)
+
 target = np.zeros((1000, 1), dtype=int)
 for i in range((1000)):
     if input_data[i][0] * input_data[i][1] > 0:
         target[i] = 1
-input_data = np.round(input_data, 1)
 input_data[input_data == 0] = 1
 
 # Inicjalizacja wag
@@ -66,7 +82,7 @@ weights_output = 2 * np.random.random((1, 4)) - 1
 
 # Parametry uczenia
 learning_rate = 0.2
-epochs = 10000
+epochs = 100
 
 # Uczenie sieci neuronowej
 weights_hidden, weights_output = backpropagation(input_data, target, weights_hidden, weights_output, learning_rate, epochs)
